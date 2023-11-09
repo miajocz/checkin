4f5f7864ac7f553e0cc830d1b7076ec637288676
IBFrost <126430979+IBFrost@users.noreply.github.com>
2023-11-09 13:13:09 -0500

    Channel Scrollbar implemented

--- src/tv-app.js
+++ src/tv-app.js
@@ -1,20 +1,20 @@
 // import stuff
-import { LitElement, html, css } from 'lit';
-import '@shoelace-style/shoelace/dist/components/dialog/dialog.js';
-import '@shoelace-style/shoelace/dist/components/button/button.js';
+import { LitElement, html, css } from "lit";
+import "@shoelace-style/shoelace/dist/components/dialog/dialog.js";
+import "@shoelace-style/shoelace/dist/components/button/button.js";
 import "./tv-channel.js";
 
 export class TvApp extends LitElement {
   // defaults
   constructor() {
     super();
-    this.name = '';
-    this.source = new URL('../assets/channels.json', import.meta.url).href;
+    this.name = "";
+    this.source = new URL("../assets/channels.json", import.meta.url).href;
     this.listings = [];
   }
   // convention I enjoy using to define the tag's name
   static get tag() {
-    return 'tv-app';
+    return "tv-app";
   }
   // LitElement convention so we update render() when values change
   static get properties() {
@@ -29,29 +29,54 @@ export class TvApp extends LitElement {
     return [
       css`
       :host {
-        display: block;
-        margin: 16px;
-        padding: 16px;
+        display: flex;
+        margin: .5rem .5rem .25rem .5rem;
+        padding: 16px;  
       }
-      `
+      .listing-container {
+        justify-self: center;
+        max-width: 1344px;
+        justify-items: left;
+        display: flex;
+        flex-direction: row;
+        flex-grow: 1;
+        flex-wrap: nowrap;
+        overflow-x: auto;
+        overflow-y: auto;
+        padding-left: .5rem;
+        padding-right: .5rem;
+        text-rendering: optimizeLegibility;
+        width: 100%;
+        margin: 0 auto;
+        position: relative;
+        animation-delay: 1s;
+        animation-duration: 1s;
+        line-height: 1.5;
+        font-size: 1em;
+      }
+      h5 {
+        font-weight: 400;
+      }
+      .
+      `,
     ];
   }
   // LitElement rendering template of your element
   render() {
     return html`
       <h2>${this.name}</h2>
-      ${
-        this.listings.map(
+      <div class="listing-container">
+        ${this.listings.map(
           (item) => html`
-            <tv-channel 
+            <tv-channel
               title="${item.title}"
               presenter="${item.metadata.author}"
               @click="${this.itemClick}"
             >
             </tv-channel>
           `
-        )
-      }
+        )}
+      </div>
       <div>
         <!-- video -->
         <!-- discord / chat - optional -->
@@ -59,19 +84,21 @@ export class TvApp extends LitElement {
       <!-- dialog -->
       <sl-dialog label="Dialog" class="dialog">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        <sl-button slot="footer" variant="primary" @click="${this.closeDialog}">Close</sl-button>
+        <sl-button slot="footer" variant="primary" @click="${this.closeDialog}"
+          >Close</sl-button
+        >
       </sl-dialog>
     `;
   }
 
   closeDialog(e) {
-    const dialog = this.shadowRoot.querySelector('.dialog');
+    const dialog = this.shadowRoot.querySelector(".dialog");
     dialog.hide();
   }
 
   itemClick(e) {
     console.log(e.target);
-    const dialog = this.shadowRoot.querySelector('.dialog');
+    const dialog = this.shadowRoot.querySelector(".dialog");
     dialog.show();
   }
 
@@ -88,11 +115,17 @@ export class TvApp extends LitElement {
   }
 
   async updateSourceData(source) {
-    await fetch(source).then((resp) => resp.ok ? resp.json() : []).then((responseData) => {
-      if (responseData.status === 200 && responseData.data.items && responseData.data.items.length > 0) {
-        this.listings = [...responseData.data.items];
-      }
-    });
+    await fetch(source)
+      .then((resp) => (resp.ok ? resp.json() : []))
+      .then((responseData) => {
+        if (
+          responseData.status === 200 &&
+          responseData.data.items &&
+          responseData.data.items.length > 0
+        ) {
+          this.listings = [...responseData.data.items];
+        }
+      });
   }
 }
 // tell the browser about our tag and class it should run when it sees it
--- src/tv-channel.js
+++ src/tv-channel.js
@@ -23,20 +23,41 @@ export class TvChannel extends LitElement {
   static get styles() {
     return css`
       :host {
-        display: inline-flex;
+        text-rendering: optimizeLegibility;
+        box-sizing: inherit;
+        display: inline-block;
+        line-height: 1.2;
+        font-size: 1em;
+        font-weight: 400;
+        min-width: 300px;
+        margin: 0;
+        padding: 0;
+        transition: all 0.25s ease-in-out;
+        overflow: hidden;
+        text-overflow: ellipsis;
       }
       .wrapper {
-        padding: 16px;
-        background-color: #eeeeee;
+        margin: .5rem;
+        padding: .5rem;
+        padding-left: 16px;
+        padding-right: 16px;
+        border-radius: 6px;
+        border-color: #4a4a4a;
+        box-shadow: 0px 0px 0px 1px #dbdbdb;
+        background-color: #ffffff;
       }
+      p {
+        font-size: 12px;
+      }
+
     `;
   }
   // LitElement rendering template of your element
   render() {
     return html`
       <div class="wrapper">
-        <h3>${this.title}</h3>
-        <h4>${this.presenter}</h4>
+        <h5>${this.title}</h5>
+        <p>${this.presenter}</p>
         <slot></slot>
       </div>  
       `;
